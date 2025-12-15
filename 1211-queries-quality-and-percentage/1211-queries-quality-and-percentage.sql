ea1d3735a02929b645deaf0a6b6186c77abe6625
Arvind Chary Padala <151894030+ArvindPadala@users.noreply.github.com>
2025-12-15 16:04:27 -0500

    Time: 337 ms (67.26%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,8 +1,1 @@
-# Write your MySQL query statement below
-
-select A.query_name, A.quality, case when B.cnt is null then 0 else round(B.cnt/A.totalcnt*100, 2) end as poor_query_percentage  from
-(select query_name, round(avg(rating/position),2) as quality, count(*) as totalcnt from Queries group by query_name) as A
-left join
-(select query_name, count(*) as cnt from Queries where rating < 3 group by query_name) as B
-on 
-A.query_name=B.query_name
+select query_name, round(avg(rating/position),2) as quality, round(sum(case when rating<3 then 1 else 0 end)/count(*)*100, 2) as poor_query_percentage from Queries group by query_name
