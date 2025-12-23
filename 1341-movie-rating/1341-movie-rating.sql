b9c10ed54ccd7e16890150819a4bb313d5987289
Arvind Chary Padala <151894030+ArvindPadala@users.noreply.github.com>
2025-12-23 17:04:51 -0500

    Time: 1184 ms (67.01%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 
 
-(select name as results from Users u join MovieRating r on u.user_id=r.user_id group by r.user_id order by count(*) desc, name limit 1)
+(select name as results from Users join MovieRating using(user_id) group by user_id order by count(*) desc, name limit 1)
 union all
-(select m.title as results from MovieRating r join Movies m on r.movie_id=m.movie_id where substring(created_at,1,7)='2020-02' group by r.movie_id order by avg(rating) desc, title limit 1)
+(select title as results from MovieRating  join Movies using(movie_id) where substring(created_at,1,7)='2020-02' group by movie_id order by avg(rating) desc, title limit 1)
 
 
