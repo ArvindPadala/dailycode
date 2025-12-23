4b6aa8e0975a9043dc5d29cdbc761f30c1bf76be
Arvind Chary Padala <151894030+ArvindPadala@users.noreply.github.com>
2025-12-23 17:11:30 -0500

    Time: 1115 ms (88.19%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -3,6 +3,6 @@
 
 (select name as results from Users join MovieRating using(user_id) group by user_id order by count(*) desc, name limit 1)
 union all
-(select title as results from MovieRating  join Movies using(movie_id) where substring(created_at,1,7)='2020-02' group by movie_id order by avg(rating) desc, title limit 1)
+(select title as results from MovieRating  join Movies using(movie_id) where extract(year_month from created_at)= 202002 group by movie_id order by avg(rating) desc, title limit 1)
 
 
