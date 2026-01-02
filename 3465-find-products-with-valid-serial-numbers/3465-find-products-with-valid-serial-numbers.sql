813e20709755674d1edcd40e4093db283f923a77
Arvind Chary Padala <151894030+ArvindPadala@users.noreply.github.com>
2026-01-02 17:01:07 -0500

    Time: 386 ms (71.09%), Space: 0B (100%) - LeetHub

--- 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
+++ 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-select * from products where description regexp '(^|[^a-zA-Z0-9])SN[0-9]{4}-[0-9]{4}([^0-9]|$)' and description like binary '%SN%' order by product_id
+select *
+from products
+where regexp_like(description ,'\\bSN[0-9]{4}-[0-9]{4}\\b','c')
+order by product_id 
