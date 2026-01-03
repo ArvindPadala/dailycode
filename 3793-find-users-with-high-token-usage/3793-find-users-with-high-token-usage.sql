f8e26615fa8b62cc87da2721cfdabece46ecd728
Arvind Chary Padala <151894030+ArvindPadala@users.noreply.github.com>
2026-01-03 14:38:44 -0500

    Time: 277 ms (0%), Space: 0B (0%) - LeetHub

--- 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.sql
+++ 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
-with A as (select user_id, count(prompt) as prompt_count, round(avg(tokens),2) as avg_tokens from prompts group by user_id having prompt_count>=3),
-
-B as (select user_id, tokens from prompts group by user_id having max(tokens)>avg(tokens))
-
-select A.user_id, A.prompt_count, A.avg_tokens from A join B on A.user_id=B.user_id order by A.avg_tokens desc, A.user_id
+SELECT
+  user_id,
+  COUNT(*) AS prompt_count,
+  ROUND(AVG(tokens), 2) AS avg_tokens
+FROM prompts
+GROUP BY user_id
+HAVING COUNT(*) >= 3
+   AND MAX(tokens) > AVG(tokens)
+ORDER BY avg_tokens DESC, user_id ASC;
