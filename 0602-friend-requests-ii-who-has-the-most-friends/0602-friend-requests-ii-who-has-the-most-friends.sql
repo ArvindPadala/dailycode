c68a881efbad06d332a2fc4a64025d9a91e8653b
Arvind Chary Padala <151894030+ArvindPadala@users.noreply.github.com>
2026-01-05 15:41:51 -0500

    Time: 318 ms (20.99%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+with A as (select requester_id as id, count(accepter_id) as c1 from RequestAccepted group by requester_id 
+union all
+select accepter_id as id, count(requester_id) as c1 from RequestAccepted group by accepter_id)
+
+select A.id, sum(A.c1) as num from A group by A.id order by num desc limit 1
+
+
+
+#select requester_id as id, count(accepter_id) as c1 from RequestAccepted group by requester_id 
+#select accepter_id as id, count(requester_id) as c1 from RequestAccepted group by accepter_id
