0edd09c92dc2223177e299b5a19840cfdd2193d9
Arvind Chary Padala <151894030+ArvindPadala@users.noreply.github.com>
2026-01-05 13:46:53 -0500

    Time: 3795 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+
+
+
+select round(sum(tiv_2016),2) as tiv_2016 from Insurance I 
+where I.tiv_2015 in (select tiv_2015 from Insurance group by tiv_2015 having count(*)>1) and (I.lat,I.lon) in (select lat,lon from Insurance group by lat,lon having count(*)=1)
