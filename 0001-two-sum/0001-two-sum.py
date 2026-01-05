01dd377cfa9a0371d3f92ed7bbdce897fe1b0848
Arvind Chary Padala <151894030+ArvindPadala@users.noreply.github.com>
2026-01-05 18:22:48 -0500

    Time: 3 ms (50.58%), Space: 18.5 MB (63.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen={}
         for i,num in enumerate(nums):
             need=target-num
             if need in seen:
                 return[seen[need],i]
             seen[num]=i
-        
