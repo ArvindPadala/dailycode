a350da61d013444f5cc25a9a667a58e7b9a4fc2a
Arvind Chary Padala <151894030+ArvindPadala@users.noreply.github.com>
2026-01-13 12:49:41 -0500

    Time: 975 ms (5.04%), Space: 0B (100%) - LeetHub

--- 3611-find-overbooked-employees/3611-find-overbooked-employees.sql
+++ 3611-find-overbooked-employees/3611-find-overbooked-employees.sql
@@ -1,15 +1,25 @@
-
-with B as (select employee_id, count(week_start) as cnt from(
+WITH heavy_weeks AS (
+  SELECT
+      employee_id,
+      DATE_SUB(meeting_date, INTERVAL WEEKDAY(meeting_date) DAY) AS week_start
+  FROM Meetings
+  GROUP BY employee_id, week_start
+  HAVING SUM(duration_hours) > 20
+),
+cnts AS (
+  SELECT
+      employee_id,
+      COUNT(*) AS meeting_heavy_weeks
+  FROM heavy_weeks
+  GROUP BY employee_id
+  HAVING COUNT(*) > 1
+)
 SELECT
-    employee_id,
-    DATE_SUB(meeting_date, INTERVAL WEEKDAY(meeting_date) DAY) AS week_start,
-    COUNT(*) AS meetings_count,
-    SUM(duration_hours) AS total_hours
-FROM Meetings
-GROUP BY employee_id, week_start
-ORDER BY employee_id, week_start) 
-as A where A.total_hours>20 
-group by employee_id)
-
-select e.employee_id, e.employee_name, e.department, B.cnt as meeting_heavy_weeks from employees e join B on e.employee_id=B.employee_id
-where B.cnt >1 order by 4 desc, 2
+    e.employee_id,
+    e.employee_name,
+    e.department,
+    c.meeting_heavy_weeks
+FROM Employees e
+JOIN cnts c
+  ON e.employee_id = c.employee_id
+ORDER BY 4 DESC, 2
